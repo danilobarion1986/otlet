cb2c94c0eaf189ce447179ec09678091563503a2
Danilo Barion Nogueira <danilo.barion@vagas.com.br>
2017-09-01 17:31:53 -0300

    Alterações na configuração e versão.

--- lib/otlet.rb
+++ lib/otlet.rb
@@ -1,8 +1,41 @@
 require "otlet/version"
+require 'yaml'
 
 # https://github.com/bitbucket-rest-api/bitbucket
+# https://stackoverflow.com/questions/6233124/where-to-place-access-config-file-in-gem
 
 module Otlet
+  # Configuration defaults
+  @config = {
+              :log_level => "verbose",
+              :min => 0,
+              :max => 99 
+            }
+
+  @valid_config_keys = @config.keys
+
+  # Configure through hash
+  def self.configure(opts = {})
+    opts.each {|k,v| @config[k.to_sym] = v if @valid_config_keys.include? k.to_sym}
+  end
+
+  # Configure through yaml file
+  def self.configure_with(path_to_yaml_file)
+    begin
+      config = YAML::load(IO.read(path_to_yaml_file))
+    rescue Errno::ENOENT
+      log(:warning, "YAML configuration file couldn't be found. Using defaults."); return
+    rescue Psych::SyntaxError
+      log(:warning, "YAML configuration file contains invalid syntax. Using defaults."); return
+    end
+
+    configure(config)
+  end
+
+  def self.config
+    @config
+  end
+
   def self.scramble(array)
     array.shuffle
 
--- lib/otlet/version.rb
+++ lib/otlet/version.rb
@@ -1,3 +1,3 @@
 module Otlet
-  VERSION = "0.1.1"
+  VERSION = "0.1.2"
 end
